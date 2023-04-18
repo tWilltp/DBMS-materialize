d2f1cee05dcfa14471e2e3db4770bb44bb72952f
tWilltp <williamjenkinsfeb@gmail.com>
2023-04-18 16:09:11 +0000

    began front end

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenave initialization
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
